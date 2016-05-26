8b1a07dee578208cd9743a023d88ce3c86a0c624
Anna Matviyenko and Vanessa Palacios Sharma <vanessa@>
2016-05-25 19:41:54 -0700

    Add finishing touches to css and index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var exampleInputName= $("input#exampleInputName").val();
-    var exampleInputLast= $("input#exampleInputLast").val();
+  $("form#blanks").submit(function(event) {
+    var exampleInputName2= $("input#exampleInputName2").val();
+    var exampleInputLast2= $("input#exampleInputLast2").val();
 
 
-    $(".exampleInputName").text(exampleInputName);
-    $(".exampleInputLast").text(exampleInputLast);
+    $(".exampleInputName2").text(exampleInputName2);
+    $(".exampleInputLast2").text(exampleInputLast2);
 
     $("#letter").show();
+
+    event.preventDefault();
       });
     });
